f6abaa9ea92ce11d1e4ed479067c340d734dcc0d
Jonatan Tierno <jonatan.tiernoalvite@telefonica.com>
2023-03-23 16:31:40 +0100

    version

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+
+import openai
+import time
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index(): 
+    print("call. key: {}".format(openai.api_key))
+    if request.method == "POST":
+        animal = request.form["animal"]
+        prompt = generate_prompt(animal)
+        print("Prompt: {}".format(prompt))
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+             messages=[
+                {"role": "system", "content": "You are a web designer."},
+                {"role": "user", "content": prompt},
+            ]
+        )
+        print("response: {}".format(response))
+        return redirect(url_for("index", result=response.choices[0].message.content))
+
+    result = request.args.get("result")
+    store_html_code(result)
+
+    return render_template("index.html", result=result)
+
+def store_html_code(text):
+    print("output: {}".format(text))
+    if text is not None:
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        filename = f"web_{timestamp}.html"
+
+        with open(filename, "w") as f:
+            f.write(text)
+
+'''
+Example prompt: A moving carousel of random images, one of them can be selected and is highlighted. Below a button says "set as wallpaper".
+'''
+def generate_prompt(animal):
+    return """Create html with embedded javascript code for the following web page: {}.""".format(animal)
